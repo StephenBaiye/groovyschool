5d0e240f77e4d06c5930265a55343003d23b52de
Stephen Baiye <stephenfamzy@gmail.com>
2019-09-16 01:20:11 +0100

    pull from upstream

---
+++ bin/main/groovyschool/UrlMappings.groovy
@@ -0,0 +1,17 @@
+package groovyschool
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view:"/index")
+        "/register"(view:"/register")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+    }
+}
